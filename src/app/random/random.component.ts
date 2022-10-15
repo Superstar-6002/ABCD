3d1db37a3b98bc90687f3f73c21111b36ea337cc
anu <nihalzz852@gmail.com>
2022-10-15 10:30:20 +0530

    Random Number

---
+++ src/app/random/random.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random',
+  templateUrl: './random.component.html',
+  styleUrls: ['./random.component.css']
+})
+export class RandomComponent implements OnInit {
+
+randomnumber:number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+generateRandom =() => {
+  this.randomnumber = Math.ceil(Math.random() * 70);
+}
+}
